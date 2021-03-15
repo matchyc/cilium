60f0d9585bd00e676296ab44483ab24b91e2292c
John Fastabend <john.fastabend@gmail.com>
2021-03-15 10:25:21 -0700

    cilium: encryptInterface requires multiple interfaces in multi-dev env
    When a node is using multiple nodes and traffic is being source routed,
    or routed some other way, across multiple netdevs we need to ensure
    our bpf_network programs are attached to all interfaces that can receive
    pod traffic.
    
    This patch adds support for users to specify multiple interfaces. In
    order to do this lift bpf_network (encryption program) compile and load
    out of init.sh and do it from go side.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- pkg/datapath/linux/node.go
+++ pkg/datapath/linux/node.go
@@ -459,8 +459,22 @@ func upsertIPsecLog(err error, spec string, loc, rem *net.IPNet, spi uint8) {
 	}
 }
 
+// getDefaultEncryptionInterface() is needed to find the interface used when
+// populating neighbor table and doing arpRequest. For most configurations
+// there is only a single interface so choosing [0] works by choosing the only
+// interface. However EKS, uses multiple interfaces, but fortunately for us
+// in EKS any interface would work so pick the [0] index here as well.
+func getDefaultEncryptionInterface() string {
+	iface := ""
+	if len(option.Config.EncryptInterface) > 0 {
+		iface = option.Config.EncryptInterface[0]
+	}
+	return iface
+}
+
 func getLinkLocalIp(family int) (*net.IPNet, error) {
-	link, err := netlink.LinkByName(option.Config.EncryptInterface)
+	iface := getDefaultEncryptionInterface()
+	link, err := netlink.LinkByName(iface)
 	if err != nil {
 		return nil, err
 	}
@@ -892,7 +906,7 @@ func (n *linuxNodeHandler) nodeUpdate(oldNode, newNode *nodeTypes.Node, firstAdd
 		if option.Config.EnableNodePort {
 			ifaceName = option.Config.DirectRoutingDevice
 		} else {
-			ifaceName = option.Config.EncryptInterface
+			ifaceName = getDefaultEncryptionInterface()
 		}
 		n.insertNeighbor(context.Background(), newNode, ifaceName, false)
 	}
@@ -1089,7 +1103,7 @@ func (n *linuxNodeHandler) createNodeIPSecInRoute(ip *net.IPNet) route.Route {
 	var device string
 
 	if option.Config.Tunnel == option.TunnelDisabled {
-		device = n.datapathConfig.EncryptInterface
+		device = n.datapathConfig.EncryptInterfaces[0]
 	} else {
 		device = linux_defaults.TunnelDeviceName
 	}
@@ -1372,7 +1386,7 @@ func (n *linuxNodeHandler) NodeNeighborRefresh(ctx context.Context, nodeToRefres
 	if option.Config.EnableNodePort {
 		ifaceName = option.Config.DirectRoutingDevice
 	} else if option.Config.EnableIPSec {
-		ifaceName = option.Config.EncryptInterface
+		ifaceName = getDefaultEncryptionInterface()
 	}
 	refreshComplete := make(chan struct{})
 	go n.refreshNeighbor(ctx, &nodeToRefresh, ifaceName, refreshComplete)
